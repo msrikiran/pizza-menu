914830e39bb7697be11fa3081b016f5baf44d377
Iamsrikiran <118716628+Iamsrikiran@users.noreply.github.com>
2024-08-24 22:22:32 +0530

    pizza items added with styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
 const pizzaData = [
   {
@@ -48,23 +49,98 @@ const pizzaData = [
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello React</h1>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
     </div>
   );
 };
 
-const Pizza = () => {
+const Header = () => {
+  // const style = { color: 'red', fontSize: '48px', textTransform: 'uppercase' };
+  const style = {};
   return (
-    <div>
-      <img src="pizzas/salamino.jpg" alt="salamino" />
-      <h1>pizza Salamino</h1>;<p>Tomato, mozarella, and pepperoni</p>
+    <header className="header">
+      <h1 style={style}>Fast react pizza co.</h1>
+    </header>
+  );
+};
+
+const Menu = () => {
+  const pizzas = pizzaData;
+  // const pizzas = [];
+  const numPizzas = pizzas.length;
+  return (
+    <div className="menu">
+      <h2>Our Menu</h2>
+      {numPizzas > 0 ? (
+        <ul className="pizzas">
+          {pizzas.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      ) : (
+        <p>We are still working on our menu, please come back later</p>
+      )}
+      {/* {numPizzas > 0 && (
+        <ul className="pizzas">
+          {pizzas.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      )} */}
+      {/* <Pizza
+        name="pizza Salamino"
+        photoName="pizzas/salamino.jpg"
+        ingredients="Tomato, mozarella, and pepperoni"
+        price={10}
+      />
+      <Pizza
+        name="Pizza Funghi"
+        photoName="pizzas/funghi.jpg"
+        ingredients="Tomato, mozarella, mushrooms, and onion"
+        price={13}
+      /> */}
     </div>
   );
 };
+const Pizza = (props) => {
+  if (props.pizzaObj.soldOut) return null;
+  return (
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+      <div>
+        <h3>{props.pizzaObj.name} </h3>
+        <p>{props.pizzaObj.ingredients} </p>
+        <span>{props.pizzaObj.price} </span>
+      </div>
+    </li>
+  );
+};
+
+const Footer = () => {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  // if (hour >= openHour && hour <= closeHour) {
+  //   alert(' we are currenlty opened');
+  // } else {
+  //   alert(' we are closed');
+  // }
+  return (
+    <footer className="footer">
+      {isOpen && (
+        <div className="order">
+          <p>We're open until {closeHour}:00</p>
+          <button className="btn">Order</button>
+        </div>
+      )}
+    </footer>
+  );
+};
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
